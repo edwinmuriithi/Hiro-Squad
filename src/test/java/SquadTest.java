74995df4a7194b3e0be8550394a7b1dee14b7ee3
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-02 14:25:25 +0300

    Check if Squad Instantiates

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SquadTest {
-
+    @Test
+    @DisplayName("Check Squad Instantiates")
+    public void creates_instanceOfSquad()
+    {
+        Squad squad = new Squad(5,"Squad A","Avengers");
+        assertTrue(squad instanceof Squad);
+    }
 }
