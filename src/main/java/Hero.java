b53813c7ae4cd1f31d4c06dda363d58760c2bcb7
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-02 12:45:22 +0300

    Add Fields to hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,2 +1,14 @@
 public class Hero {
+
+    private String name;
+    private int age;
+    private String special_power;
+    private  String weakness;
+
+    public Hero(String name, int age, String special_power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.special_power = special_power;
+        this.weakness = weakness;
+    }
 }
