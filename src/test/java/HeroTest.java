a2e544a125c42f8688dac57d9c7a1501e9824b29
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-02 13:08:16 +0300

    Checks if Hero Instantiates

--- src/test/java/HeroTest.java
+++ src/test/java/HeroTest.java
@@ -1,5 +1,14 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HeroTest {
+    @Test
+    @DisplayName("Checks if Hero Instantiates")
+    public void createsInstanceOfHero{
+        Hero hero = new Hero("Hulk",30,"Strength","Anger issues");
+        assertEquals(true, hero instanceof Hero);
+    }
 
 }
