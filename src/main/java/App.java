7ce14aa46cb43fea66f18cae754825f5848b85f5
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-02 12:37:34 +0300

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
-}
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
+
+        BasicConfigurator.configure();
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+    }
+    }
