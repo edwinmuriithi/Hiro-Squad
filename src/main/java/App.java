9f1186db12c94bf611e89ad344b5fe3fe8cdf63b
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-03 09:15:37 +0300

    Update dependancies

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+
 public class App {
 
     static int getHerokuAssignedPort() {
@@ -8,14 +13,20 @@ public class App {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4568; //return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String[] args) {
 
-////Go to Hero Page
-//        get("/hero/:id", (req, res) -> {
-//            Map<String, Object> model = new HashMap<>();
-//            return new ModelAndView(model, "hero.hbs");
-//        }, new HandlebarsTemplateEngine());
+        //home
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        //hero
+        get("/hero", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
     }
     }
